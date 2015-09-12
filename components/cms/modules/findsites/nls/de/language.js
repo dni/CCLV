6a5deebbcb302d702a57ab9166c4935a9cf4412f
dni <dni.khr@gmail.com>
2015-09-12 23:33:03 +0200

    add site module

---
+++ components/cms/modules/findsites/nls/de/language.js
@@ -0,0 +1,21 @@
+define({
+    "navigation": "Funde",
+    "details": "Fund Details",
+    "emptyMessage": "Der Fund existiert nicht mehr",
+    "newModel": "hat neuen Fund erstellt",
+    "deleteModel": "hat Fund gelöscht",
+    "updateModel": "hat Fund bearbeitet",
+    "attributes": {
+        "title": "Titel",
+        "category": "Kategorie",
+        "subcategory": "Subkategorie",
+        "detector": "Detektor",
+        "type": "Fundtyp",
+        "date": "Fundatum",
+        "description": "Beschreibung",
+        "teaserimage": "Vorschaubild",
+        "place": "Aufbewahrungsort",
+        "lng": "Longitude",
+        "lat": "Latitude"
+    }
+});
---
+++ components/cms/modules/findsites/nls/language.js
@@ -0,0 +1,24 @@
+define({
+  "root": {
+    "navigation": "Finds",
+    "details": "Find details",
+    "attributes":  {
+      "title": "Title",
+      "category": "Category",
+      "subcategory": "Subcategory",
+      "detector": "Detector",
+      "type": "Find Type",
+      "date": "Find date",
+      "teaserimage": "Teaserimage",
+      "description": "Description",
+      "place": "Place of storage",
+      "lng": "Longitude",
+      "lat": "Latitude"
+    },
+    "emptyMessage": "The find does not exist anymore",
+    "newModel": "created new find",
+    "deleteModel": "deleted find",
+    "updateModel": "updated find"
+  },
+  "de": true
+});
